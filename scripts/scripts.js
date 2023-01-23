5e7cb575482fb6ae10d9ca159fad0b14c287cebb
DMergy <mer21040@byui.edu>
2023-01-23 12:53:29 -0800

    chamber

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,3 +8,4 @@ document.querySelector("#lastMod").textContent = lastMod;
 
 //const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
 //document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
