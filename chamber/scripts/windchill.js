83166fbc078a377899c8a6f409f539bbb60d7b16
DMergy <mer21040@byui.edu>
2023-03-21 03:12:51 -0700

    week six

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,19 @@
+let t = parseFloat(document.querySelector("#t").textContent);
+let s = parseFloat(document.querySelector("#s").textContent);
+let windchill = "";
+
+if (t <= 50 && s > 3) {
+  windchill = windChill(t, s);
+  windchill = Math.round(35.74 + (0.6215 * t) - (35.75 * (s ** 0.16)) + (0.4275 * t * (s ** 0.16)));
+} 
+else {
+  windchill = "N/A";
+}
+// output
+document.querySelector("#w").innerHTML = windchill;
+
+function windChill(temp, speed) {
+  return "SUCCESS";
+}
+
+
