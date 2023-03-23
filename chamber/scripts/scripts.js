edcb5b87c3e6924b8a43a46f612ae5faa68c5465
DMergy <mer21040@byui.edu>
2023-03-22 21:03:49 -0700

    discover

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -52,3 +52,31 @@ if (dayofweek == weekday[1] || dayofweek == weekday [2]) {
 else {
     banner.style.display = 'none';
 }
+
+//* Discover Page Lazy Loading //*
+
+var imagesToLoad = document.querySelectorAll('img[data-src]');
+var loadImages = function(image) {
+	image.setAttribute('src', image.getAttribute('data-src'));
+	image.onload = function() {
+		image.removeAttribute('data-src');
+	};
+};
+if('IntersectionObserver' in window) {
+	var observer = new IntersectionObserver(function(items, observer) {
+		items.forEach(function(item) {
+			if(item.isIntersecting) {
+				loadImages(item.target);
+				observer.unobserve(item.target);
+			}
+		});
+	});
+	imagesToLoad.forEach(function(img) {
+		observer.observe(img);
+	});
+}
+else {
+	imagesToLoad.forEach(function(img) {
+		loadImages(img);
+	});
+}
