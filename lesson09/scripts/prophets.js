69330edf0889f0d9dc871f0ac12b01769ab76126
DMergy <mer21040@byui.edu>
2023-03-31 05:24:59 -0700

    prophets

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -8,39 +8,38 @@ async function getProphetData(url) {
 //    console.table(data.prophets);  
 // note that we reference the prophet array of the data object given the structure of the json file
 }
+
+getProphetData();
   
 const displayProphets = (prophets) => {
     const cards = document.querySelector("div.cards"); // select the output container element
 
     prophets.forEach((prophet) => {
-    // Create elements to add to the div.cards element
-    let card = document.createElement("section");
-    let h2 = document.createElement("h2");
-    let portrait = document.createElement("img");
-    let bDate = document.createElement("p");
-    let bPlace = document.createElement("p");
+        // Create elements to add to the div.cards element
+        let card = document.createElement("section");
+        let h2 = document.createElement("h2");
+        let bDate = document.createElement("p");
+        let bPlace = document.createElement("p");
+        let portrait = document.createElement("img");
 
-    // Build the h2 content out to show the prophet's full name - finish the template string
-    h2.textContent = `${prophet.name} ${prophet.lastname}`;
+        // Build the h2 content out to show the prophet's full name - finish the template string
+        h2.textContent = `${prophet.name} ${prophet.lastname}`;
+        bDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        bPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
 
-    bDate.textContent = `${prophet.birthdate}`;
-    bPlace.textContent = `${prophet.birthplace}`;
+        // Build the image portrait by setting all the relevant attributes
+        portrait.setAttribute("src", prophet.imageurl);
+        portrait.setAttribute("alt", "Portrait of ${prophet.name} ${prophet.lastname}");
+        portrait.setAttribute("loading", "lazy");
+        portrait.setAttribute("width", "340");
+    
 
-    // Build the image portrait by setting all the relevant attributes
-    portrait.setAttribute("src", prophet.imageurl);
-    portrait.setAttribute("alt", "Portrait of ${prophet.name} ${prophet.lastname}");
-    portrait.setAttribute("loading", "lazy");
-    portrait.setAttribute("width", "340");
-   
+        // Append the section(card) with the created elements
+        card.appendChild(h2);
+        card.appendChild(bDate);
+        card.appendChild(bPlace);
+        card.appendChild(portrait);
 
-    // Append the section(card) with the created elements
-    card.appendChild(h2);
-    card.appendChild(portrait);
-    card.appendChild(bDate);
-    card.appendChild(bPlace);
-
-    cards.appendChild(card);
+        cards.appendChild(card);
     })
 }
-
-getProphetData();
