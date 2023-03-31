0b804020aeae745986f987a72b63c04d4108e879
DMergy <mer21040@byui.edu>
2023-03-31 14:27:58 -0700

    directory

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -3,10 +3,9 @@ const url = "json/directory.json";
 async function getSeasideData() {
     const response = await fetch(url);
     const data = await response.json();
-    
+    //console.table(data.seaside);
     displaySeaside (data.seaside);
-      
-// note that we reference the prophet array of the data object given the structure of the json file
+    
 }
 
 getSeasideData();
@@ -14,29 +13,34 @@ getSeasideData();
 const displaySeaside = (seaside) => {
     const cards = document.querySelector("div.cards"); // select the output container element
 
-    seaside.forEach((prophet) => {
+    seaside.forEach((seaside) => {
         // Create elements to add to the div.cards element
         let card = document.createElement("section");
         let logo = document.createElement("img");
+        let company = document.createElement("h3");
         let address = document.createElement("p");
         let phone = document.createElement("p");
         let web = document.createElement("a");
         
 
         // Build the h2 content out to show the prophet's full name - finish the template string
+        company.textContent = `${seaside.company}`;
         address.textContent = `${seaside.address}`;
         phone.textContent = `${seaside.phone}`;
         
         // Build the image portrait by setting all the relevant attributes
-        web.setAttribute("href", seaside.web);
+        web.setAttribute("href", `${seaside.web}`);
+        web.textContent = "Website - Click Here";
+
         logo.setAttribute("src", seaside.logo);
         logo.setAttribute("alt", "Business Logo");
         logo.setAttribute("loading", "lazy");
-        logo.setAttribute("width", "340");
+        logo.setAttribute("width", "250");
     
 
         // Append the section(card) with the created elements
         card.appendChild(logo);
+        card.appendChild(company);
         card.appendChild(address);
         card.appendChild(phone);
         card.appendChild(web);
