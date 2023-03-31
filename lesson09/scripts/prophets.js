3b267c687ac0b44418acd550be00ef30a5693de3
DMergy <mer21040@byui.edu>
2023-03-31 06:10:38 -0700

    prophets

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,11 +1,11 @@
-const url = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 
-async function getProphetData(url) {
+async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-
+    
     displayProphets(data.prophets);
-//    console.table(data.prophets);  
+      
 // note that we reference the prophet array of the data object given the structure of the json file
 }
 
